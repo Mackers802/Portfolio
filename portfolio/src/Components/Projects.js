45cecd44c4d9b2c4a0a4a54df78544bc454716e0
Mackenzie Yandow <Mackers802@gmail.com>
2021-01-26 18:01:55 -0800

    added build

--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -13,10 +13,10 @@ export default function Projects() {
     // }
     return (
         <>
-        <div className="projectCont">
-            <h1>Projects</h1>
-            <h2>Coming soon to a browser near you</h2>
-        </div>
+            <div className="projectCont">
+                <h1>Projects</h1>
+                <h2>Coming soon to a browser near you</h2>
+            </div>
         </>
     )
 }
