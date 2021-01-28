5d36ab031157d7adaeff26f1f90801512ac92762
Mackenzie Yandow <Mackers802@gmail.com>
2021-01-28 11:39:42 -0800

    added mario pest control iframe

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,15 +6,22 @@ export default function Projects() {
         <>
             <div className="projectCont">
                 <h1>Projects</h1>
+
+            <h2>Designed using React</h2>
                 <div className="iFrames">
-                    <iframe style={{width: "350px", height: "450px"}} src="https://memeitup.netlify.app/" title="Meme Gen"></iframe>
+                    <iframe style={{width: "320px", height: "425px", borderRadius: "30px", borderStyle: "doublegroove", borderWidth: "8px", borderColor: " rgba(125, 120, 120, 0.361)" }} src="https://memeitup.netlify.app/" title="Meme Gen"></iframe>
                 </div>
                 <div className="iFrames">
-                    <iframe style={{width: "350px", height: "450px"}} src="https://reviewthisrandommovie.netlify.app/" title="Random Movie Reviewer"></iframe>
+                    <iframe style={{width: "320px", height: "425px", borderRadius: "30px", borderStyle: "groove", borderWidth: "8px", borderColor: " rgba(125, 120, 120, 0.361)" }} src="https://reviewthisrandommovie.netlify.app/" title="Random Movie Reviewer"></iframe>
                 </div>
-                {/* <div className="iFrames">
-                    <iframe style={{width: "325px", height: "450px"}} src="https://memeitup.netlify.app/" title="Meme Gen"></iframe>
-                </div> */}
+                <hr></hr>
+                <br></br>
+                <div>
+                <h2>Designed using HTML / CSS / Javascript projects</h2>
+                <div className="iFrames">
+                    <iframe style={{width: "320px", height: "425px", borderRadius: "30px", borderStyle: "groove", borderWidth: "8px", borderColor: " rgba(125, 120, 120, 0.361)" }} src="https://mariopestcontrol.netlify.app/" title="Mario Pest Control"></iframe>
+                </div>
+                </div>
 
                 
             </div>
